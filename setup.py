6908a39798ef63092ad802bb8bbabe2ab36f5732
Hidden <schuck2345@gmail.com>
2021-09-13 21:33:16 +0200

    Pip installation fixes

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 from distutils.core import setup
 
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  name = 'DiscAPI',         # How you named your package folder (MyLib)
+  packages = ['DiscAPI'],   # Chose the same as "name"
+  version = '0.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  description = 'A simple to use Python Library to make all kinds of discord bot.',   # Give a short description about your library
+  author = 'HiddenStorm',                   # Type in your name
+  author_email = 'schuck2345@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/Hiddenstorm/DiscAPI',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords = ['Python', 'Discord', 'Simple'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'asyncio',
+          'websocket-client'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.9',
   ],
 )
